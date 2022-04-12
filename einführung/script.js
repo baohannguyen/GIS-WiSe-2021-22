9f617b9c3fd7cc1d6a034e829235f2e1592161e1
baohannguyen <bao.han.nguyen@hs-furtwangen.de>
2022-04-12 08:39:41 +0200

    einführung

---
+++ einführung/script.js
@@ -0,0 +1,10 @@
+console.log("hello world");
+
+alert("Das ist eine Warnung");
+
+const divElement = document.getElementById("div-element");
+
+console.log(divElement); 
+
+divElement.textContent = "Das ist der neue Wert"; //damit kann man z.b den inhalt im div element ändern
+divElement.classList.add("test-classe");
